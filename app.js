1c75c40ba978ed0394f85cfda1d43fc46104d6f7
Alwin Nedrum <nedrum@gmail.com>
2022-01-13 17:23:43 +0100

    Added button and Calculator components

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
-class Calculator extends React.Component {
-  render() {
-    return <h1>Hello Calculator</h1>
-  }
+const numberStr = [
+  'ZERO',
+  'ONE',
+  'TWO',
+  'THREE',
+  'FOUR',
+  'FIVE',
+  'SIX',
+  'SEVEN',
+  'EIGHT',
+  'NINE',
+]
+
+function Button(props) {
+  return (
+    <button className={'btn ' + props.class} id={props.id} value={props.value}>
+      {props.value}
+    </button>
+  )
+}
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <h1>See you Calculator</h1>
+      <div className="numbers">
+        {numberStr.map((element, i) => {
+          if (i > 0 && i <= 3) {
+            return <Button key={i} class="row1" id={element} value={i} />
+          }
+
+          if (i > 3 && i <= 6) {
+            return <Button key={i} class="row2" id={element} value={i} />
+          }
+          if (i > 6 && i <= 9) {
+            return <Button key={i} class="row3" id={element} value={i} />
+          }
+        })}
+        <Button key={0} class="row4" id={numberStr[0]} value="0" />
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(
