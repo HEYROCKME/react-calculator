10a589dd5b51fb17766c864dd5e8c96a0be6244f
Alwin Nedrum <nedrum@gmail.com>
2022-01-13 18:53:12 +0100

    added styling

--- app.js
+++ app.js
@@ -13,30 +13,53 @@ const numberStr = [
 
 function Button(props) {
   return (
-    <button className={'btn ' + props.class} id={props.id} value={props.value}>
+    <button className={'btn ' + props.group} id={props.id} value={props.value}>
       {props.value}
     </button>
+  )
+}
+
+function Display(props) {
+  return (
+    <div id="display" text="0">
+      {props.text}
+    </div>
   )
 }
 
 function Calculator() {
   return (
     <div className="calculator">
-      <h1>See you Calculator</h1>
-      <div className="numbers">
-        {numberStr.map((element, i) => {
-          if (i > 0 && i <= 3) {
-            return <Button key={i} class="row1" id={element} value={i} />
-          }
+      <h3 className="device">Calc101</h3>
+      <Display text="See you Calculator" />
 
-          if (i > 3 && i <= 6) {
-            return <Button key={i} class="row2" id={element} value={i} />
-          }
-          if (i > 6 && i <= 9) {
-            return <Button key={i} class="row3" id={element} value={i} />
-          }
-        })}
-        <Button key={0} class="row4" id={numberStr[0]} value="0" />
+      <div className="interface-btn">
+        <div className="numbers">
+          {numberStr.map((element, i) => {
+            if (i > 0 && i <= 3) {
+              return <Button key={i} group="row1" id={element} value={i} />
+            }
+
+            if (i > 3 && i <= 6) {
+              return <Button key={i} group="row2" id={element} value={i} />
+            }
+            if (i > 6 && i <= 9) {
+              return <Button key={i} group="row3" id={element} value={i} />
+            }
+          })}
+
+          <Button id="CLEAR" value="CLR" />
+          <Button key={0} class="row4" id={numberStr[0]} value="0" />
+          <Button id="decimal" value="." />
+          <Button id="equals" value="=" />
+        </div>
+        <div class="operators">
+          <Button id="ADD" value="+" />
+          <Button id="ADD" value="-" />
+          <Button id="ADD" value="x" />
+          <Button id="ADD" value="/" />
+          <Button id="ADD" value="Flip" />
+        </div>
       </div>
     </div>
   )
